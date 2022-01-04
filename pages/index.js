b82cf353563ab604c9dd88b2e4fef247aa2f0a08
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2022-01-04 03:38:39 +0000

    aboutSkills section done

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <meta name="description" content="Nafis Handoko portfolio website" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css" />
       </Head>
 
       <header>
@@ -27,10 +28,36 @@ export default function Home() {
           <p>
             I am a Frontend Developer and UI Designer that recently learning React and Redux for my current projects. Even though i'm new, i'll keep trying to improve my skills as time goes on. If you need me, feel free to contact me
           </p>
-          <a className={styles.cta}>Contact Me</a>
+          <a href="#" className={styles.cta}>Contact Me</a>
         </div>
       </header>
-      <section></section>
+      <section className={styles.aboutSkills}>
+        <div className={styles.about}>
+          <h2>About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id fermentum fermentum nunc. Pellentesque a ante mattis molestie lorem arcu. Scelerisque neque eget ac a. Nunc tortor, donec imperdiet at. Nec amet laoreet nibh in tristique purus, interdum pulvinar sed. Suspendisse eros, at et porttitor accumsan tempor neque sed. Egestas posuere tempus nec elit fringilla pharetra fermentum. Mattis nibh ante in nulla pellentesque sed blandit. Tristique in nunc vulputate sed varius nulla aliquam magna. Tempus in porta aliquet sollicitudin. Vitae arcu magnis amet nascetur proin lacus, facilisis.
+          </p>
+        </div>
+        <div className={styles.skills}>
+          <h2>My Skills</h2>
+          <div className={styles.skillList}>
+            <a href="#" className={styles.skill}><i class="devicon-html5-plain-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-css3-plain-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-sass-original colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-bootstrap-plain-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-tailwindcss-plain colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-javascript-plain colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-jquery-plain-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-react-original-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-nextjs-original-wordmark"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-php-plain colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-mysql-plain-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-python-plain-wordmark colored"></i></a>
+            <a href="#" className={styles.skill}><i class="devicon-flask-original-wordmark"></i></a>
+            <a href="#" className={styles.skill}> <i class="devicon-git-plain-wordmark colored"></i></a>
+          </div>
+        </div>
+      </section>
 
       <footer className={styles.footer}>
         <a
