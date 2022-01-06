929156cbadea575ddaa8319bc64ae6aa01175a90
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2022-01-06 12:02:51 +0000

    relayout portItem

--- components/Portfolio/index.js
+++ components/Portfolio/index.js
@@ -1,10 +1,10 @@
 import styles from './index.module.css'
 
-export default function Portfolio(props){
+function Portfolio2(props){
   return (
     <div className={styles.portItem}>
       <div className={styles.portImgDiv}>
-        <img className={styles.portImg} src="https://via.placeholder.com/150/FCDAB7/1D2D50 ?Text=Digital.com" />
+        <img className={styles.portImg} src="https://via.placeholder.com/150/FCDAB7/1D2D50?Text=Digital.com" />
       </div>
       <div className={styles.portDesc}>
         <h3>{props.title}</h3>
@@ -19,3 +19,24 @@ export default function Portfolio(props){
     </div>
   )
 }
+
+function Portfolio(props){
+  return (
+    <div className={styles.portItem2}>
+      <figure className={styles.portFigure}>
+        <img className={styles.portImg2} src="https://via.placeholder.com/1920x1280/FCDAB7/1D2D50?Text=Digital.com" />
+        <figcaption className={styles.portFigcaption}>
+          <h4>{props.title}</h4>
+          <p className={styles.portDesc}>{props.desc}</p>
+          <div className={styles.portDemo}>
+            <a className={styles.demoIcon} href={props.repo}><i className="bi bi-github"></i></a>
+            <a className={styles.demoIcon} href={props.demo}><i className="bi bi-box-arrow-up-right"></i></a>
+          </div>
+        </figcaption>
+        <div className={styles.overlay}></div>
+      </figure>
+    </div>
+  )
+}
+
+export default Portfolio;
